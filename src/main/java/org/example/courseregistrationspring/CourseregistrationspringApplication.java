07400fee96212a8dcc068f3ed6ccd3d8b8d42c4f
ABISHEK-0404 <abishekdaielrajv.22mech@kongu.edu>
2025-07-21 23:07:09 +0530

    Initial commit

---
+++ src/main/java/org/example/courseregistrationspring/CourseregistrationspringApplication.java
@@ -0,0 +1,13 @@
+package org.example.courseregistrationspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseregistrationspringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseregistrationspringApplication.class, args);
+    }
+
+}
