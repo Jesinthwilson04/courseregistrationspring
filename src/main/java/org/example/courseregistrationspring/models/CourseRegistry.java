c7b3ce0b6bcc861e8bdb1fe0a70c09fc395a5135
ABISHEK-0404 <abishekdaielrajv.22mech@kongu.edu>
2025-07-22 16:09:49 +0530

    CourseRegistry.java is added

--- src/main/java/org/example/courseregistrationspring/models/CourseRegistry.java
+++ src/main/java/org/example/courseregistrationspring/models/CourseRegistry.java
@@ -1,4 +1,18 @@
 package org.example.courseregistrationspring.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
 public class CourseRegistry {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String emailId;
+    private String courseName;
+
 }
