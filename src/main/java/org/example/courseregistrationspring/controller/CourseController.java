022994b735b65b93538a26fffd333a83fa697e4f
ABISHEK-0404 <abishekdaielrajv.22mech@kongu.edu>
2025-07-27 22:58:50 +0530

    updated

--- src/main/java/org/example/courseregistrationspring/controller/CourseController.java
+++ src/main/java/org/example/courseregistrationspring/controller/CourseController.java
@@ -6,6 +6,7 @@ import org.example.courseregistrationspring.services.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -24,5 +25,10 @@ public class CourseController {
         return courseService.getReg();
     }
     @PostMapping("/register")
+    public void registerStdCourses(@RequestParam("name") String name,
+                                   @RequestParam("emailid") String emailid,
+                                   @RequestParam("coursename") String coursename){
 
+
+    }
 }
